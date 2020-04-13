f80290097146840a92a2f4495614f6eb11a3a43d
Richie Lee <richie@bzzzt.io>
2020-04-13 13:11:04 +0100

    make_db gets pokemon formes images and stats

--- scripts/make_db.py
+++ scripts/make_db.py
@@ -6,7 +6,7 @@ import urllib
 import os
 import re
 import pickle
-
+import time
 '''
 Copyright (c) 2016-2018 Vanessa Sochat
 Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -25,7 +25,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 '''
-
+uid = 1
 base = "http://pokemondb.net"
 pokemon_index = "%s/pokedex/national" %(base)
 
@@ -56,44 +56,69 @@ for poke in pokemon:
     url = "%s%s" %(base,link)
 
     # Pokemon Individual Page
+    # Enumerate as value of i is required for identifying correct stats table and determining when to set the names of formes
+    # Some pokemon are missing artwork as not officially released, so added try/catch in this case
     response = requests.get(url)
     psoup = BeautifulSoup(response.text)
-    img_div = psoup.find('div',attrs={"class":"grid-col span-md-6 span-lg-4 text-center"})
-    img = img_div.findChild('img').get('src')
-    ext = os.path.splitext(os.path.basename(img))[-1]
-    image_file = "%s/%s%s" %(image_dir,pid,ext)
-    if os.path.exists(image_file):
-        os.remove(image_file)
-    os.system("wget %s -O %s --no-check-certificate --secure-protocol=TLSv1" %(img,image_file))
+    img_div = psoup.findAll('div',attrs={"class":"grid-col span-md-6 span-lg-4 text-center"})
+    for i, forme in enumerate(img_div):
+        img = None
+        try:
+            img = forme.findChild('img').get('src')
+        except:
+                print("no artwork found!")
+                break
+        print(img)
+        ext = os.path.splitext(os.path.basename(img))[-1]
+        # keep first entries with original names, all other entries use their formes
+        if not i==0:
+            forme_name = forme.findChild('img').get('alt').split(' artwork by Ken Sugimori', 1)[0]
+        else:
+            forme_name = pokemon_name
+        forme_name = forme_name.replace("♂","-m").replace("♀","-f")
+        image_file = "%s/%s%s%s" %(image_dir,pid,forme_name.replace(" ", ""),ext)
+        if os.path.exists(image_file):
+            os.remove(image_file)
+        os.system("wget %s -O %s --no-check-certificate --secure-protocol=TLSv1" %(img,image_file))
 
-    # Make ascii
+        # Make ascii
     
-    ascii = handle_image_conversion(image_file,new_width=30)
+        ascii = handle_image_conversion(image_file,new_width=30)
     
-    # Stats
-    stats = {}
-    save_stats = ["Japanese"]
-    table = psoup.find("tbody")
-    for row in table.findAll('tr'):
-        header = row.find('th').getText()
-        content = row.find('td').getText()
-        if header in save_stats:
-            stats[header] = content
-        elif header == "Weight":
-            stats[header] = float(re.sub("lbs|[)]","",content.partition("(")[-1]))
-        elif header == "Height":
-            stats[header] = float(content.partition('m')[0])
-        elif header in ["Abilities","Type"]:
-            stats[header] = [x.getText().lower() for x in row.findAll('a')]
-
+        # Stats
+        # some Pokemon do not have stats as they are not yet released, so added if float check
+        # No class to pick out stat tables so have to pick out nth table that will have stats for Pokemon and formes
+        # First table will be stats for primary forme
+        # Ftats for subsequent formes will 5th, 9th etc so multiply the i by 4 to get correct stat table for this forme
+        stats = {}
+        save_stats = ["Japanese"]
+        tables = psoup.findAll('table', attrs={"class": "vitals-table"})
+        table = (tables[i*4])
+        for row in table.findAll('tr'):
+            header = row.find('th').getText()
+            content = row.find('td').getText()
+            if header in save_stats:
+                stats[header] = content
+            elif header == "Weight":
+                weight = re.sub("lbs|[)]","",content.partition("(")[-1])
+                if(weight==float):
+                    stats[header] = float(weight)
+            elif header == "Height":
+                height = re.sub("m","",content.partition('m')[0])
+                if(height==float):
+                    stats[header] = float(height)
+            elif header in ["Abilities","Type"]:
+                stats[header] = [x.getText().lower() for x in row.findAll('a')]
     # Save it!
-    pokemons[pid] = {"id":pid,
-                     "name":pokemon_name,
-                     "link":url,
-                     "ascii":ascii}
+        pokemons[uid] = {"id":pid,
+                         "name":forme_name,
+                         "link":url,
+                         "ascii":ascii}
 
-    for stat_name,stat_value in stats.items():
-        pokemons[pid][stat_name.lower()] = stat_value
+        for stat_name,stat_value in stats.items():
+            pokemons[uid][stat_name.lower()] = stat_value
+
+        uid = uid + 1
 
 output_file = "%s/pokemons.json" %(database_dir)
 save_json(pokemons,output_file)
